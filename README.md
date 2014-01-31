6d2465e6c38812fcc6752895566dbd13d1142d8b
Chad Lee <git@chadly.net>
2014-01-31 16:00:12 -0600

    Initial commit
    Including v1 of binding handler

---
+++ README.md
@@ -0,0 +1,11 @@
+#Knockout Inline Confirm Binding
+
+This is a simple knockout binding that allows you to…
+
+##Install with [Bower](http://bower.io/)
+
+```
+bower install knockout-inline-confirm
+```
+
+Then add `knockout.inline-confirm.js` to your project.
---
+++ knockout.inline-confirm.js
@@ -0,0 +1,53 @@
+define(["jquery", "knockout"], function ($, ko) {
+	ko.bindingHandlers.inlineConfirm = {
+		init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+
+			var span = $('<span></span>').addClass('button__text');
+			span.appendTo($(element));
+
+			$(element).click(function () {
+
+				var textValues = ko.utils.unwrapObservable(valueAccessor());
+				var submitFunction = ko.utils.unwrapObservable(allBindingsAccessor().submitFunction);
+				var timeOut = ko.utils.unwrapObservable(allBindingsAccessor().confirmTimeout) || 3000;
+				var disabled = ko.utils.unwrapObservable(allBindingsAccessor().disable);
+
+				if (!disabled) {
+					var stepIndex = textValues.indexOf(span.text());
+					if (stepIndex < textValues.length - 2) {
+						element.resetTimer = setTimeout(function () {
+							span.text(textValues[stepIndex]);
+						}, timeOut);
+
+						span.text(textValues[stepIndex + 1]);
+					}
+					else if (stepIndex === textValues.length - 2) {
+
+						if (element.resetTimer) {
+							clearTimeout(element.resetTimer);
+							element.resetTimer = null;
+						}
+
+						$(element).addClass("is-busy");
+						span.text(textValues[textValues.length - 1]);
+
+						if (submitFunction) {
+							if (typeof (submitFunction) !== 'function') {
+								throw new typeError('expected typeof "submitFunction" to be "function"');
+							}
+							submitFunction.call(ko.dataFor(this));
+						}
+					}
+				}
+
+				//let the click continue
+				return !submitFunction && !disabled;
+			});
+		},
+		update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+			var span = $(element).find('.button__text');
+			span.text(ko.utils.unwrapObservable(valueAccessor())[0]);
+			$(element).removeClass("is-busy");
+		}
+	};
+});
